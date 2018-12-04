b13bf451487ce9ddac6ab8be5431aecd0fe22150
Mneff19 <mpneff19@gmail.com>
2018-12-03 20:23:42 -0700

    started homepage styling- industry cards need some work

--- src/client/components/homePage/header.js
+++ src/client/components/homePage/header.js
@@ -7,7 +7,7 @@ export default class header extends Component {
                 <li><a class="active" href="/homePage">Home</a></li>
                 <li><a href="#news">Services</a></li>
                 <li><a href="/industries">Industries</a></li>
-                <li><a href="#contact">Careers</a></li>
+                <li><a href="#contact">Schedule A Time</a></li>
                 <li><a href="#about">Insights</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="#about">Contact</a></li>
--- src/client/components/homePage/homePage.js
+++ src/client/components/homePage/homePage.js
@@ -12,54 +12,54 @@ export default class homePage extends Component {
             <Header/>,
             <div className='city'>
                 <ImageLink
-                    imageurl='https://www.thetimes.co.uk/travel/s3/growthtravel-prod/uploads/2017/11/Hong_Kong_Cedric009-1500x792.jpg'
+                    imageurl = 'https://www.muralswallpaper.com/app/uploads/saturated-london-cityscape-city-plain.jpg'
                 /> 
             
             </div>,           
 
-            <div className='home-page'>
-                
+            <div className ='servicesHolder'>
+                <div className='title-services'>
+                    Services
+                </div>
+
+                <div className='services-columns'>
+                        <div className='services'>
+                            <h1>TAXES</h1>
+                            <ImageLink
+                                imageurl='https://bfba.com/wp-content/uploads/2017/10/bfba_Tax-Icon-Blue.png'
+                                linkUrl='/'
+                            />
+                        </div>
+
+                        <div className='services'>
+                            <h1>ASSURANCE</h1>
+                            <ImageLink
+                                imageurl='https://bfba.com/wp-content/uploads/2017/10/bfba_Assuance-Icon-Blue.png'
+                                linkUrl='/'
+                            />
+                        </div>
+
+                        <div className='services'>
+                            <h1>ADVISORY</h1>
+                            <ImageLink
+                                imageurl='https://bfba.com/wp-content/uploads/2017/10/bfba_Advisory-Icon-Blue.png'
+                                linkUrl='/'
+                            />
+                        </div>
+                </div>
             </div>,
 
-            <div className='title-services'>
-                Services
+            <div className='industriesHolder'>
+                <div className='title-industries'>
+                    Industries
+                </div>
+
+                <div className='industries-cards'>
+                    <IndustryCard/>
+                    <IndustryCard />
+                    <IndustryCard />
+                </div>
             </div>,
-
-            <div className='survaces-columns'>
-                    <div className='survaces'>
-                        <h1>TAXES</h1>
-                        <ImageLink
-                            imageurl='https://bfba.com/wp-content/uploads/2017/10/bfba_Tax-Icon-Blue.png'
-                            linkUrl='/'
-                        />
-                    </div>
-
-                    <div className='survaces'>
-                        <h1>ASSURANCE</h1>
-                        <ImageLink
-                            imageurl='https://bfba.com/wp-content/uploads/2017/10/bfba_Assuance-Icon-Blue.png'
-                            linkUrl='/'
-                        />
-                    </div>
-
-                    <div className='survaces'>
-                        <h1>ADVISORY</h1>
-                        <ImageLink
-                            imageurl='https://bfba.com/wp-content/uploads/2017/10/bfba_Advisory-Icon-Blue.png'
-                            linkUrl='/'
-                        />
-                    </div>
-            </div>,
-
-            <div className='title-industries'>
-                Industries
-            </div>,
-
-            <IndustryCard/>,
-            <br/>,
-            <IndustryCard />,
-            <br />,
-            <IndustryCard />,
                    
            <Footer />
         ]
