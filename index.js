4262b3f463b5f4a5c3f853254ee57927cb59bff1
Student THS Students <thstu@TCSD.tooelesd.org>
2018-12-05 14:56:06 -0700

    Changes to Allow Deployment to Heroku

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const os = require("os");
+const path = require('path');
 
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, '/dist/')));
+
 app.get("/api/getUsername", (req, res) =>
   res.send({ username: os.userInfo().username })
 );
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
 // Start the server on port 8080
 app.listen(8080, () => console.log("Listening on port 8080!"));
