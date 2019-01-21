537b62cde672d7d389933cb1fb63bbdd06455d15
ColtonNorthcutt <cnor7334@gmail.com>
2019-01-21 15:32:46 -0700

    Finished industries page text

--- src/client/components/industriesPage.js
+++ src/client/components/industriesPage.js
@@ -15,7 +15,7 @@ export default class industries extends Component {
                 </div>
 
                 <div className='body-text-center'>
-                    At Accuracy Accounting we help our clients with all financing from all industries. With over 20 years of business, we have extensive experiance in many specific industries allowing us to provide the best possible service to our clients. With so much experiance under our belt, we are experts in providing advice that meets your every accounting need in these industries.
+                    At Accuracy we help our clients with all financing from all industries. With over 20 years of business, we have extensive experiance in many specific industries allowing us to provide the best possible service to our clients. With so much experiance under our belt, we are experts in providing advice that meets your every accounting need in these industries.
                     <br /><br /><br /><br />
                 </div>
 
@@ -24,7 +24,7 @@ export default class industries extends Component {
                         imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
                     />
                 </div>
-
+ 
                 <div className='subheading'>
                     Construction
                 </div>
@@ -54,24 +54,222 @@ export default class industries extends Component {
                 </div>
                 
                 <div className='subheading'>
-                    Construction
+                    Real Estate
                 </div>
 
                 <div className='body-text'>
-                    We have decades worth of experience with this industry allowing us to put our vast knowledge to work for you. We understand the importance of this industry and take care in  providing the best possible assistance. We have experts who can meet with you and help regardless of your needs.
+                    The real estate cycle can be long and complicated, and we understand every aspect no matter where you are in the cycle. Whether you are starting a new project, in the middle of an existing project, or considering an exit strategy, our team of trained professionals can help you at every turn. We understand this delicate process and know how plans are likely to change over time and we have the resources to make every step as smooth as possible.                    <br /> <br />
                     <br /> <br />
-                    No matter what you struggling with, from financial reporting to simple questions, ask one of our construction members for help. We would be glad to assist you with your every need.
-                    <br /> <br />
-                    We can assist you with all you construction needs including:
+                    We can aid you with all you real estate needs including:
                 </div>
                 <div className='industries-bullets'>
                     <br />
-                    <li>Bonding and Bandking relationships</li> <br />
-                    <li>Cash flow management</li> <br />
-                    <li>Construction software recommendations</li> <br />
-                    <li>Multi-stage operations</li> <br />
-                    <li>General strategic planning</li>
+                    <li>Passive activity rules</li> <br />
+                    <li>Capital gains planning</li> <br />
+                    <li>Financial statement audits and reviews</li> <br />
+                    <li>1031 exchanges</li> <br />
+                    <li>Business succession</li>
                     <br /><br /><br /><br />
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Not-for-Profit
+                </div>
+
+                <div className='body-text'>
+                    Accuracy is interested in making its community better, which is why it supports non-profit businesses. We work for every type of non-profit businesses including museums, schools and more. As your businesses become larger and demand increases, our experts can give you solutions to help your business continue to grow.
+                    <br /> <br />
+                    Our non-profit services include:
+                </div>
+                <div className='industries-bullets'>
+                    <br />
+                    <li>Financial statement audit and accounting services</li> <br />
+                    <li>Single audits</li> <br />
+                    <li>Tax consulting and tax return preparation</li> <br />
+                    <li>Compliances audits</li> <br />
+                    <li>Strategic planning</li>
+                    <br /><br /><br /><br />
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Professional Services
+                </div>
+
+                <div className='body-text'>
+                    Accuracy provides aid to all business, big or small. Our experienced team has the knowledge to help you in any industry regardless of the service you provide. Our services are customized to each client individually and help you plan for the future.                    
+                    <br /> <br />
+                    Accuracy offers specialized services for professional service firms including:
+                </div>
+                <div className='industries-bullets'>
+                    <br />
+                    <li>Business valuations</li> <br />
+                    <li>Succession planning</li> <br />
+                    <li>Consulting to help increase the value of your company</li> <br />
+                    <li>Cash management strategies</li> <br />
+                    <li>Sale of your business</li>
+                    <br /><br /><br /><br />
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Energy
+                </div>
+
+                <div className='body-text'>
+                    The energy industry has a completely different set of issues and challenges that other industries don’t have to face. At Accounting, our advisors understand these issues, and can analyze energy trends to help their clients turn ideas into opportunities. This industry is rapidly changing, and our professionals can help you keep up to help to find you place in this complex industry.
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Tansportation
+                </div>
+
+                <div className='body-text'>
+                    Keeping up with the transportation industry can be difficult to do by yourself. You must have everything down to a science or you and other companies can start losing money. Accuracy can help you manage your business and keep everything running smoothly so you can maximize your profits. 
+                    <br /> <br />
+                    With the increasing compaction in this industry, we can help your company come out on top by helping you integrate new technologies to reduce costs and improve operations. Our goal is to help you run a more efficient business. 
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Agriculture
+                </div>
+
+                <div className='body-text'>
+                    Agriculture often requires specialized methods and knowledge. Our long history of accounting gives us the awareness and insight our clients need to give them first rate services to put you on the path to success. 
+	                <br /> <br />
+                    We work with ranchers, farmers, vineyards, and many other agricultural products to provide them with our industry specific methods to decrease tax liabilities. We know to help all business in this vital industry no matter what you produce. 
+                    <br /> <br />
+                    Our Agricultural experts can assist you with services including:
+                </div>
+                <div className='industries-bullets'>
+                    <br />
+                    <li>Cash Flow Management</li> <br />
+                    <li>Audit services</li> <br />
+                    <li>Tax Return Preparation</li> <br />
+                    <li>Fixed Asset Tracking</li> <br />
+                    <li>Quick Books Consulting</li>
+                    <br /><br /><br /><br />
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Property Management
+                </div>
+
+                <div className='body-text'>
+                    With our vast experience in real estate, we can transfer our knowledge and add more specific to the property management industry. Our bookkeeping services can provide back office support and allow you to focus on expanding you company.
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Wholesale & Distribution
+                </div>
+
+                <div className='body-text'>
+                    Accounting can help you manage every aspect of your company from inventory or financial reporting. Our team of highly trained experts can provide you with the strategies to ensure success. We can also provide you with ideas to reduce costs and minimize daily operational issues or help you to put your ideas into action. No matter what wholesale and distribution issue you have we have the knowledge and experience to ensure you keep accurate order fulfillment and delivery.
+                </div>
+
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Manufacturing
+                </div>
+
+                <div className='body-text'>
+                    Manufacturing is an old but important industry. We help our clients understand the changing and complex law surrounding this industry. We assist our clients in becoming more successful and growing by finding ways to cunt costs and maximize efficiency while minimizing costs.
+                    <br /> <br />
+                    Our Manufacturing services include:
+                </div>
+                <div className='industries-bullets'>
+                    <br />
+                    <li>Projections and forecasts</li> <br />
+                    <li>Profit contribution analysis</li> <br />
+                    <li>Tax consulting and preparation</li> <br />
+                    <li>Audit and accounting services</li> <br />
+                    <li>Gross margin improvements</li>
+                    <br /><br /><br /><br />
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Communications
+                </div>
+
+                <div className='body-text'>
+                    Communication is a booming industry as is growing ever more important. Because of the varying circumstances, such as location and technology, it can be incredibly difficult to navigate. With over 20 years of experience, we have seen communication develop, and have become experts in this field. We can help business solve issues unique to this industry.
+                    <br /> <br />
+                    Our communications experts can aid you in our services including:
+                </div>
+                <div className='industries-bullets'>
+                    <br />
+                    <li>Budgeting</li> <br />
+                    <li>Regulatory cost and pricing</li> <br />
+                    <li>Succession planning</li> <br />
+                    <li>Business valuations</li> <br />
+                    <li>Regulatory accounting</li>
+                    <br /><br /><br /><br />
+                </div>
+
+                <div className='industries-image'>
+                    <ImageLink
+                        imageurl='https://cdnassets.hw.net/8b/16/ad10b7644b379890958adca8b519/construction.jpg'
+                    />
+                </div>
+
+                <div className='subheading'>
+                    Benefit Plans
+                </div>
+
+                <div className='body-text'>
+                    We have a dedicated team of auditors that understand the complex procedure of performing employee benefit pan audits. This team has the essential skills required to ensure the appropriate standards and regulation as made that could impact you employee’s benefit plan. Accuracy is devoted to achieving the best quality audit possible.
                 </div>
             </div>,
 
